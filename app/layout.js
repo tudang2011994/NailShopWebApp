0f25fc6af78391674d44787877402a1eb80e3eca
Tu Dang <tudang2011994@gmail.com>
2025-06-14 12:10:41 -0700

    add google tag

--- app/layout.js
+++ app/layout.js
@@ -48,28 +48,27 @@ export default function RootLayout({ children }) {
           `}
         </Script> */}
 
-        <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','AW-16852708460');
-                `,
-              }}
-            />
-      </Head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=AW-16852708460"
+              <iframe src="https://www.googletagmanager.com/gtag/js?id=AW-16852708460"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16852708460'););
+            `,
+          }}
+        />
+
+      </Head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ClientProvider>
           <div id="root">{children}</div>
         </ClientProvider>
